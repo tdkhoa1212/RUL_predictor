3cfe9d3499dfcc06bf2b15edf6584024095f33aa
tdkhoa1212 <tdkhoa1212@gmail.com>
2023-05-07 21:39:55 +0700

    a

--- utils/load_XJTU_data.py
+++ utils/load_XJTU_data.py
@@ -151,12 +151,12 @@ else:
 
   if opt.encoder:
     EC_XJTU_path = join(opt.save_dir, 'XJTU.h5')
-    if exists(EC_XJTU_path) == False:
-      test_1D, test_2D, test_extract, test_label_RUL, test_label_Con = getting_data(saved_dir, opt.test_bearing, opt)
-      train_1D, train_2D, train_extract, train_label_RUL, train_label_Con = getting_data(saved_dir, opt.train_bearing, opt)
-      s_0, s_1, s_2 = train_1D.shape
-      train_1D = train_1D.reshape((s_0, s_2, s_1))
-      train_EC(train_1D, 'XJTU', opt)
+    # if exists(EC_XJTU_path) == False:
+    test_1D, test_2D, test_extract, test_label_RUL, test_label_Con = getting_data(saved_dir, opt.test_bearing, opt)
+    train_1D, train_2D, train_extract, train_label_RUL, train_label_Con = getting_data(saved_dir, opt.train_bearing, opt)
+    s_0, s_1, s_2 = train_1D.shape
+    train_1D = train_1D.reshape((s_0, s_2, s_1))
+    train_EC(train_1D, 'XJTU', EC_XJTU_path, opt)
 
   # Saving the converted data ==================================================================================
   if os.path.exists(join(saved_dir, 'Bearing1_4_data_1d.npy')) == False:
--- utils/train_encoder.py
+++ utils/train_encoder.py
@@ -1,13 +1,16 @@
 from model.autoencoder import autoencoder_model
+from os.path import exists
 from os.path import join
 import tensorflow as tf
 
-def train_EC(train_data, type_, opt):
-    print('\n' + 10*'-' + f'TRAIN AUTOENCODER MODEL WITH {type_} data' + '\n' + 10*'-')
+def train_EC(train_data, type_, weights, opt):
+    print('\n' + 10*'-' + f' TRAIN AUTOENCODER MODEL WITH {type_} data ' + 10*'-')
     model = autoencoder_model(type_)
     model.summary()
     model.compile(optimizer=tf.keras.optimizers.Adam(1e-3),
                   loss=tf.keras.losses.Huber(reduction=tf.keras.losses.Reduction.NONE))
+    if exists(weights):
+      model.load_weights(weights)
     model.fit(train_data, train_data,
                 epochs=opt.EC_epochs,
                 shuffle=True,
