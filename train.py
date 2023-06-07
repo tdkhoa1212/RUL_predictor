d70c5bcbc1bd8056bbacb4a53b00348d7bdebe58
tdkhoa1212 <tdkhoa1212@gmail.com>
2023-06-07 10:13:33 +0700

    a

--- train.py
+++ train.py
@@ -28,10 +28,10 @@ def parse_opt(known=False):
    
     parser.add_argument('--save_dir',       default='/content/drive/MyDrive/Khoa/results/RUL', type=str)
     parser.add_argument('--data_type',      default=['2d', '1d', 'extract'], type=list, help='shape of data. They can be 1d, 2d, extract')
-    parser.add_argument('--train_bearing',  default=['Bearing1_1', 'Bearing1_2', 'Bearing2_1','Bearing2_2','Bearing3_1','Bearing3_2'], type=str, nargs='+')   
-    parser.add_argument('--test_bearing',   default=['Bearing1_3', 'Bearing1_4', 'Bearing1_5', 'Bearing2_3', 'Bearing2_4', 'Bearing2_5', 'Bearing3_3', 'Bearing3_4', 'Bearing3_5'], type=str, nargs='+')
+    parser.add_argument('--train_bearing',  default=['Bearing1_2', 'Bearing1_3', 'Bearing1_4','Bearing1_5','Bearing1_6','Bearing1_7'], type=str, nargs='+')   
+    parser.add_argument('--test_bearing',   default=['Bearing1_1'], type=str, nargs='+')
     parser.add_argument('--condition',      default=None, type=str, help='c_1, c_2, c_3, c_all')
-    parser.add_argument('--type',           default='XJTU', type=str, help='PHM, XJTU')
+    parser.add_argument('--type',           default='PHM', type=str, help='PHM, XJTU')
     parser.add_argument('--case',           default='case2', type=str, help='case1, case2')
     parser.add_argument('--scaler',         default=None, type=str)
     parser.add_argument('--main_dir_colab', default='/content/drive/MyDrive/Khoa/data_new/data', type=str)
