5707a3595b149f8ad5339f7f9904ae70c491542d
tdkhoa1212 <tdkhoa1212@gmail.com>
2023-06-26 19:24:50 +0700

    a

--- utils/tools.py
+++ utils/tools.py
@@ -213,12 +213,6 @@ def extract_feature_image(df, opt, type_data, feature_name='horiz accel', type_=
       EC_PHM_path = join(opt.save_dir, f'{type_}.h5')
       model.load_weights(EC_PHM_path)
 
-      df = np.expand_dims(df.reshape(df.shape[1], df.shape[0]), axis=0)
-      raw_signal_filter = np.where(df>0, 1, -1)
-      df = raw_signal_filter * model.predict(df*raw_signal_filter, verbose = 0, batch_size = 32)
-      df = np.squeeze(df)
-      df = df.reshape(df.shape[1], df.shape[0])
-
     if type_ == 'PHM':
       DATA_POINTS_PER_FILE=2560
       if feature_name == 'horiz accel':
@@ -287,6 +281,13 @@ def convert_to_image(name_bearing, opt, type_data, time=None, type_=None):
               coef_v = extract_feature_image(df, opt, type_data, feature_name='vert accel', type_=type_)
               x_ = np.concatenate((coef_h, coef_v), axis=-1)
 
+              if type_data != '2d':
+                x_ = np.expand_dims(x_.reshape(x_.shape[1], x_.shape[0]), axis=0)
+                raw_signal_filter = np.where(x_>0, 1, -1)
+                x_ = raw_signal_filter * model.predict(x_*raw_signal_filter, verbose = 0, batch_size = 32)
+                x_ = np.squeeze(x_)
+                x_ = x_.reshape(x_.shape[1], x_.shape[0])
+
               if type_data == '1d' and opt.PCAlabel == True:
                 data['y'].append(compute_PCA(x_))
 
@@ -307,6 +308,13 @@ def convert_to_image(name_bearing, opt, type_data, time=None, type_=None):
               coef_v = extract_feature_image(df, opt, type_data, feature_name='Vertical_vibration_signals', type_=type_)
               x_ = np.concatenate((coef_h, coef_v), axis=-1)
 
+              if type_data != '2d':
+                x_ = np.expand_dims(x_.reshape(x_.shape[1], x_.shape[0]), axis=0)
+                raw_signal_filter = np.where(x_>0, 1, -1)
+                x_ = raw_signal_filter * model.predict(x_*raw_signal_filter, verbose = 0, batch_size = 32)
+                x_ = np.squeeze(x_)
+                x_ = x_.reshape(x_.shape[1], x_.shape[0])
+                
               if type_data == '1d' and opt.PCAlabel == True:
                 data['y'].append(compute_PCA(x_))
                 
